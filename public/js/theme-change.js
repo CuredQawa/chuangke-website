4bec6eba8517f7597b4075bc7896a6b6e878cbff
Disappear_Blank <3394307156@qq.com>
2025-08-13 18:28:48 +0800

    localStorage -> sessionStorage

--- public/js/theme-change.js
+++ public/js/theme-change.js
@@ -1,4 +1,4 @@
-// 页面加载时检查 localStorage 中是否有主题设置
+// 页面加载时检查 sessionStorage 中是否有主题设置
 function applyTheme(theme) {
     const element = document.documentElement;
     const themeSwitch = document.getElementById("theme-switch");
@@ -22,7 +22,7 @@ function getCurrentTheme() {
     const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
 
     // 如果有存储值就用存储值，否则默认是系统主题
-    return localStorage.getItem('theme') || systemTheme;
+    return sessionStorage.getItem('theme') || systemTheme;
 }
 
 // 确保DOM加载完成后应用主题
@@ -44,7 +44,7 @@ initializeTheme();
 // 监听系统主题变化
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
     // 只有在没有手动设置主题时才跟随系统主题变化
-    if (!localStorage.getItem('theme')) {
+    if (!sessionStorage.getItem('theme')) {
         const newTheme = event.matches ? 'dark' : 'light';
         applyTheme(newTheme);
     }
@@ -59,12 +59,12 @@ function switchTheme() {
         if (themeSwitch) {
             themeSwitch.setAttribute('class', 'iconfont icon-yueliang');
         }
-        localStorage.setItem('theme', 'light'); // 存入 light
+        sessionStorage.setItem('theme', 'light'); // 存入 light
     } else {
         documentElement.setAttribute('data-theme', 'dark');
         if (themeSwitch) {
             themeSwitch.setAttribute('class', 'iconfont icon-ai250');
         }
-        localStorage.setItem('theme', 'dark'); // 存入 dark
+        sessionStorage.setItem('theme', 'dark'); // 存入 dark
     }
 }
