8127a1311c1ba26a304d49aef272f769e38fa130
za-songguo <zasongguo@outlook.com>
2025-08-24 16:08:41 +0800

    编辑账户

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
-/**
- * 应用程序入口文件
- * 设置Express服务器并启动监听
- */
-
 import express from 'express';
 import router from './router.js'
 import dbMiddleware from './middleware/database.js';
@@ -14,15 +9,9 @@ const app = express();
 app.use(express.json());
 app.use(dbMiddleware);
 app.use(express.static("public"));
-// Cookies 解析
 app.use(cookieParser());
 app.use("/", router);
 
-/**
- * 启动服务器并监听指定端口
- */
 app.listen(RUNNING_PORT, () => {
     console.log(`Running on localhost:${RUNNING_PORT}`);
 })
-
-// TODO 编辑账户页面
